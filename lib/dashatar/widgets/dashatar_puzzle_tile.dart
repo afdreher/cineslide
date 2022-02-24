4bce74042b8b98ddae738253d12aea6191771f61
Andrew F. Dreher <afdreher@agavix.com>
2022-02-24 14:11:16 -0600

    Adds animated desaturation and corner numbers
    Animates tiles to have blurred corners & desaturate when in the incorrect position

--- lib/dashatar/widgets/dashatar_puzzle_tile.dart
+++ lib/dashatar/widgets/dashatar_puzzle_tile.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 // Package imports:
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:themed/themed.dart';
 
 // Project imports:
 import 'package:cineslide/audio_control/audio_control.dart';
@@ -22,6 +23,7 @@ import 'package:cineslide/typography/text_styles.dart';
 import 'package:cineslide/colors/colors.dart';
 import 'package:cineslide/settings/settings.dart';
 import 'package:cineslide/corner_triangle/corner_triangle.dart';
+import 'package:cineslide/typography/outlined_text.dart';
 
 abstract class _TileSize {
   static double small = 75;
@@ -66,6 +68,9 @@ class DashatarPuzzleTileState extends State<DashatarPuzzleTile>
   late AnimationController _controller;
   late Animation<double> _scale;
 
+  /// [Animation] to desaturate the tile color
+  late Animation<double> _decolorize;
+
   @override
   void initState() {
     super.initState();
@@ -76,6 +81,13 @@ class DashatarPuzzleTileState extends State<DashatarPuzzleTile>
     );
 
     _scale = Tween<double>(begin: 1, end: 0.94).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0, 1, curve: Curves.easeInOut),
+      ),
+    );
+
+    _decolorize = Tween<double>(begin: 0.0, end: -0.95).animate(
       CurvedAnimation(
         parent: _controller,
         curve: const Interval(0, 1, curve: Curves.easeInOut),
@@ -117,57 +129,109 @@ class DashatarPuzzleTileState extends State<DashatarPuzzleTile>
 
     final canPress = hasStarted && (puzzleIncomplete || puzzlePending);
 
+    final isCorrect = widget.tile.isInCorrectPosition;
+
     return AudioControlListener(
       audioPlayer: _audioPlayer,
-      child: AnimatedAlign(
-        alignment: FractionalOffset(
-          (widget.tile.currentPosition.x - 1) / (size - 1),
-          (widget.tile.currentPosition.y - 1) / (size - 1),
+      child: AnimatedTile(
+          tile: widget.tile,
+          size: size,
+          movementDuration: movementDuration,
+          canPress: canPress,
+          controller: _controller,
+          scale: _scale,
+          theme: theme,
+          isCorrect: isCorrect,
+          audioPlayer: _audioPlayer),
+    );
+  }
+}
+
+class AnimatedTile extends StatelessWidget {
+  const AnimatedTile({
+    Key? key,
+    required this.size,
+    required this.tile,
+    required this.movementDuration,
+    required this.canPress,
+    required this.controller,
+    required this.scale,
+    required this.theme,
+    required this.isCorrect,
+    required AudioPlayer? audioPlayer,
+  })  : _audioPlayer = audioPlayer,
+        super(key: key);
+
+  final int size;
+  final Tile tile;
+  final Duration movementDuration;
+
+  final bool canPress;
+  final AnimationController controller;
+
+  final Animation<double> scale;
+
+  final DashatarTheme theme;
+  final bool isCorrect;
+  final AudioPlayer? _audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedAlign(
+      // The AnimatedAlign is responsible for the animated 'shift' in the tile
+      // pieces.  This should probably be replaced with an animated build to
+      // transition both the position and the color / animation.  However,
+      // the onEnd may be sufficient to start playing / transitioning the color
+      alignment: FractionalOffset(
+        (tile.currentPosition.x - 1) / (size - 1),
+        (tile.currentPosition.y - 1) / (size - 1),
+      ),
+      duration: movementDuration,
+      curve: Curves.easeInOut,
+      child: ResponsiveLayoutBuilder(
+        small: (_, child) => SizedBox.square(
+          key: Key('dashatar_puzzle_tile_small_${tile.value}'),
+          dimension: _TileSize.small,
+          child: child,
         ),
-        duration: movementDuration,
-        curve: Curves.easeInOut,
-        child: ResponsiveLayoutBuilder(
-          small: (_, child) => SizedBox.square(
-            key: Key('dashatar_puzzle_tile_small_${widget.tile.value}'),
-            dimension: _TileSize.small,
-            child: child,
-          ),
-          medium: (_, child) => SizedBox.square(
-            key: Key('dashatar_puzzle_tile_medium_${widget.tile.value}'),
-            dimension: _TileSize.medium,
-            child: child,
-          ),
-          large: (_, child) => SizedBox.square(
-            key: Key('dashatar_puzzle_tile_large_${widget.tile.value}'),
-            dimension: _TileSize.large,
-            child: child,
-          ),
-          child: (_) => MouseRegion(
-            onEnter: (_) {
-              if (canPress) {
-                _controller.forward();
-              }
-            },
-            onExit: (_) {
-              if (canPress) {
-                _controller.reverse();
-              }
-            },
-            child: ScaleTransition(
-              key: Key('dashatar_puzzle_tile_scale_${widget.tile.value}'),
-              scale: _scale,
-              child: PuzzleTileButton(
-                tile: widget.tile,
-                theme: theme,
-                onPressed: canPress
-                    ? () {
-                        context.read<PuzzleBloc>().add(TileTapped(widget.tile));
-                        unawaited(_audioPlayer?.replay());
-                      }
-                    : null,
-                child: Image.asset(
-                  theme.dashAssetForTile(widget.tile),
-                ),
+        medium: (_, child) => SizedBox.square(
+          key: Key('dashatar_puzzle_tile_medium_${tile.value}'),
+          dimension: _TileSize.medium,
+          child: child,
+        ),
+        large: (_, child) => SizedBox.square(
+          key: Key('dashatar_puzzle_tile_large_${tile.value}'),
+          dimension: _TileSize.large,
+          child: child,
+        ),
+        child: (_) => MouseRegion(
+          onEnter: (_) {
+            if (canPress) {
+              controller.forward();
+            }
+          },
+          onExit: (_) {
+            if (canPress) {
+              controller.reverse();
+            }
+          },
+          child: ScaleTransition(
+            key: Key('dashatar_puzzle_tile_scale_${tile.value}'),
+            scale: scale,
+            child: PuzzleTileButton(
+              key: Key('dashatar_puzzle_tile_${tile.value}_button'),
+              tile: tile,
+              theme: theme,
+              duration: movementDuration,
+              isCorrect: isCorrect,
+              onPressed: canPress
+                  ? () {
+                      context.read<PuzzleBloc>().add(TileTapped(tile));
+                      unawaited(_audioPlayer?.replay());
+                    }
+                  : null,
+              child: Image.asset(
+                theme.dashAssetForTile(tile),
               ),
             ),
           ),
@@ -178,27 +242,52 @@ class DashatarPuzzleTileState extends State<DashatarPuzzleTile>
 }
 
 class PuzzleTileButton extends StatelessWidget {
-  const PuzzleTileButton(
+  PuzzleTileButton(
       {Key? key,
       required this.tile,
       required this.theme,
+      this.isCorrect = false,
       this.onPressed,
+      required this.duration,
       this.child})
-      : super(key: key);
+      : childKey = Key('puzzle_tile_${tile.value}_animation'),
+        super(key: key);
 
   final Tile tile;
 
-  final VoidCallback? onPressed;
-
   final PuzzleTheme theme;
+  final bool isCorrect;
+
+  final Duration duration;
+
+  final VoidCallback? onPressed;
+  final Key childKey;
 
   final Widget? child;
+
+  final _forwardTween = Tween<double>(begin: -0.95, end: 0.0);
+  final _reverseTween = Tween<double>(begin: 0.0, end: -0.95);
 
   @override
   Widget build(BuildContext context) {
     Widget? theChild = child;
-
-    print('Building PuzzleTileButton with tile #${tile.value}');
+    if (theChild != null) {
+      // Animate this change!
+      theChild = TweenAnimationBuilder(
+        key: childKey,
+        child: theChild,
+        tween: isCorrect
+            ? _forwardTween
+            : _reverseTween,
+        duration: duration,
+        builder: (_, double value, Widget? myChild) {
+          return ChangeColors(
+            saturation: value,
+            child: myChild!,
+          );
+        },
+      );
+    }
 
     return Semantics(
       button: true,
@@ -220,7 +309,7 @@ class PuzzleTileButton extends StatelessWidget {
           padding: EdgeInsets.zero,
           alignment: Alignment.topLeft,
         ),
-        onPressed: this.onPressed,
+        onPressed: onPressed,
         child: Stack(
           children: <Widget>[
             if (theChild != null) theChild,
@@ -232,7 +321,6 @@ class PuzzleTileButton extends StatelessWidget {
                   if (!context.read<SettingsBloc>().state.showTileNumbers) {
                     return Container();
                   }
-
                   return Stack(
                     children: [
                       ClipPath(
@@ -247,18 +335,21 @@ class PuzzleTileButton extends StatelessWidget {
                       ),
                       FractionallySizedBox(
                         widthFactor: 1.0,
-                      heightFactor: 1.0,
-                      child: CornerTriangle(
-                        corner: Corner.topLeft,
-                        color: theme.titleColor.withOpacity(0.25),
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 10, top: 5),
-                          child: Text(
-                            tile.value.toString(),
-                            style: TextStyle(color: theme.titleColor),
+                        heightFactor: 1.0,
+                        child: CornerTriangle(
+                          clipBehavior: Clip.antiAlias,
+                          corner: Corner.topLeft,
+                          color: theme.titleColor.withOpacity(0.25),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10, top: 5),
+                            child: OutlinedText(
+                              tile.value.toString(),
+                              style: TextStyle(color: theme.titleColor),
+                              strokeColor: theme.backgroundColor,
+                              strokeWidth: 2,
+                            ),
                           ),
                         ),
-                      ),
                       ),
                     ],
                   );
