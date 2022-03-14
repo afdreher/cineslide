eaf4ce8c880fc14c1ceb0b53639f50bb344822c7
Andrew F. Dreher <afdreher@agavix.com>
2022-03-14 06:51:04 -0500

    Styles difficulty selection and adds about screen
    Styles difficult selection.  Adds super quick aboutScreen (unstyled) to acknowledge all the OSS.

--- lib/menu/vertical_menu_page.dart
+++ lib/menu/vertical_menu_page.dart
@@ -1,3 +1,6 @@
+import 'dart:math' as math;
+import 'dart:ui';
+
 // Flutter imports:
 import 'package:flutter/material.dart';
 
@@ -24,36 +27,38 @@ class VerticalMenuPage extends StatelessWidget {
       await showDialog<int>(
           context: context,
           builder: (BuildContext context) {
-            return SimpleDialog(
-              children: <Widget>[
-                SimpleDialogOption(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(
-                      '/puzzle',
-                      arguments: PuzzleArguments(theme: theme, squareCount: 3),
-                    );
+            return Center(
+              child: SizedBox(
+                width: 200,
+                child: FilmStrip.vertical(
+                  key: const Key('menu_puzzle_difficulty_selection'),
+                  startingNumber: 9,
+                  frameText: (int i) {
+                    if (i == 9) {
+                      return 'DIFFICULTY';
+                    }
+                    return '';
                   },
-                  child: const Text('Easy: 3'),
+                  aspect: 0.7,
+                  perfCount: 4,
+                  unexposedFilmColor: FilmColors.unexposedColor.withAlpha(220),
+                  exposedFilmColor: FilmColors.exposedColor.withAlpha(180),
+                  children: <Widget>[
+                    _DifficultyButton(
+                        difficulty: context.l10n.puzzleDifficultyEasy,
+                        squareCount: 3,
+                        theme: theme),
+                    _DifficultyButton(
+                        difficulty: context.l10n.puzzleDifficultyNormal,
+                        squareCount: 4,
+                        theme: theme),
+                    _DifficultyButton(
+                        difficulty: context.l10n.puzzleDifficultyHard,
+                        squareCount: 5,
+                        theme: theme),
+                  ],
                 ),
-                SimpleDialogOption(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(
-                      '/puzzle',
-                      arguments: PuzzleArguments(theme: theme, squareCount: 4),
-                    );
-                  },
-                  child: const Text('Normal: 4'),
-                ),
-                SimpleDialogOption(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(
-                      '/puzzle',
-                      arguments: PuzzleArguments(theme: theme, squareCount: 5),
-                    );
-                  },
-                  child: const Text('Hard: 5'),
-                ),
-              ],
+              ),
             );
           });
     }
@@ -71,32 +76,41 @@ class VerticalMenuPage extends StatelessWidget {
       frameText: (_) => 'FLUTTER DEVPOST',
       children: [
         SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                OutlinedText(
-                  context.l10n.applicationName,
-                  strokeColor: const Color.fromRGBO(88, 82, 75, 1.0),
-                  strokeWidth: 2.0,
-                  style: Theme.of(context).textTheme.headline2!.copyWith(
-                        color: titleFrameTextColor,
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const Divider(
-                  indent: 20,
-                  endIndent: 20,
-                  color: titleFrameTextColor,
-                ),
-                const Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      'Flutter Puzzle Challenge',
-                      style: TextStyle(color: titleFrameTextColor),
-                    )),
-              ],
+          child: TextButton(
+            onPressed: () {
+              showAboutDialog(
+                context: context,
+                applicationName: context.l10n.applicationName,
+                applicationVersion: '1.0.0',
+              );
+            },
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  OutlinedText(
+                    context.l10n.applicationName,
+                    strokeColor: const Color.fromRGBO(88, 82, 75, 1.0),
+                    strokeWidth: 2.0,
+                    style: Theme.of(context).textTheme.headline2!.copyWith(
+                          color: titleFrameTextColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  const Divider(
+                    indent: 20,
+                    endIndent: 20,
+                    color: titleFrameTextColor,
+                  ),
+                  const Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        'Flutter Puzzle Challenge',
+                        style: TextStyle(color: titleFrameTextColor),
+                      )),
+                ],
+              ),
             ),
           ),
         ),
@@ -150,5 +164,72 @@ class VerticalMenuPage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class _DifficultyButton extends StatelessWidget {
+  const _DifficultyButton({
+    Key? key,
+    required this.difficulty,
+    required this.squareCount,
+    required this.theme,
+  }) : super(key: key);
+
+  final String difficulty;
+  final int squareCount;
+  final CinematicTheme theme;
+
+  @override
+  Widget build(BuildContext context) {
+    //return LayoutBuilder(builder: (BuildContext context, BoxConstraints constrains) {
+    return Center(
+        child: Stack(
+      children: <Widget>[
+        Positioned.fill(
+          top: -40,
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Transform.rotate(
+              angle: -math.pi / 10,
+              child: ImageFiltered(
+                imageFilter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+                child: Text(
+                  squareCount.toString(),
+                  style: const TextStyle(
+                    color: Colors.white60,
+                    fontFamily: 'SourceCodePro',
+                    fontSize: 120,
+                  ),
+                ),
+              ),
+            ),
+            //   strokeColor: const Color(0xFFDDDDDD),
+            // ),
+          ),
+        ),
+        Positioned.fill(
+          child: TextButton(
+            child: OutlinedText(
+              difficulty.toUpperCase(),
+              style: const TextStyle(
+                color: Color(0xBBF0F0F0),
+                fontFamily: 'SourceCodePro',
+                fontSize: 24,
+              ),
+              strokeWidth: 1,
+            ),
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                '/puzzle',
+                arguments:
+                    PuzzleArguments(theme: theme, squareCount: squareCount),
+              );
+            },
+          ),
+        ),
+      ],
+    ));
+
+    // });
   }
 }
